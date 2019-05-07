ddfbf7f2d1dd01839364b0f8e3945e93c7ec8ded
Brian Palmer <brian.palmer@dreamsandcode.com>
2019-05-07 03:24:25 -0400

    Blog Posts now showing on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = (props) => {
+  const postList = props.data.allMarkdownRemark;
+  return (
   <Layout>
     <SEO
       title="Home"
@@ -21,14 +23,36 @@ const IndexPage = () => (
         "Blog",
       ]}
     />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {postList.edges.map(({ node }, i) => (
+        <Link to={node.fields.slug} className="link" >
+          <div className="post-list">
+            <h1>{node.frontmatter.title}</h1>
+            <span>{node.frontmatter.date}</span>
+            <p>{node.excerpt}</p>
+          </div>
+        </Link>
+      ))}
   </Layout>
-)
+  )
+}
 
 export default IndexPage
+
+export const listQuery = graphql`
+  query ListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          fields{
+            slug
+          }
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM Do YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+  `
