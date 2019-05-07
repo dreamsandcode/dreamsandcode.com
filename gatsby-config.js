30a85aff73866fd5581085cb3f2c9f751e9e8a36
Brian Palmer <brian.palmer@dreamsandcode.com>
2019-05-07 02:16:59 -0400

    Created geneic blog post template

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,14 @@ module.exports = {
         icon: `src/images/dreamsandcode-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-transformer-remark`,
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
   ],
 }
